0a33c78da041a36276bc1e814d0baf03939128b5
KingKhan911 <arman1914khan@gmail.com>
2026-09-05 21:26:43 +0530

    Add shared editorial research metadata

---
+++ src/lib/editorial.js
@@ -0,0 +1,16 @@
+export const RESEARCH_REVIEW_DATE = 'September 5, 2026';
+
+export const SOURCES = {
+  pewTipping:
+    'https://www.pewresearch.org/2023/11/09/services-americans-do-and-dont-tip-for-and-how-much/',
+  toastTipping:
+    'https://pos.toasttab.com/blog/data/restaurant-tipping-trends',
+  dolTippedWages:
+    'https://www.dol.gov/agencies/whd/state/minimum-wage/tipped',
+  dolFactSheet15:
+    'https://www.dol.gov/agencies/whd/fact-sheets/15-tipped-employees-flsa',
+  irsTips:
+    'https://www.irs.gov/businesses/small-businesses-self-employed/tip-recordkeeping-and-reporting',
+  emilyPost:
+    'https://emilypost.com/advice/general-tipping-guide',
+};
