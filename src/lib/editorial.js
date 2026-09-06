24f61fafa15cb420b359ff06b49efbe9dfde6a59
KingKhan911 <arman1914khan@gmail.com>
2026-09-06 13:02:14 +0530

    Add international guidance sources

--- src/lib/editorial.js
+++ src/lib/editorial.js
@@ -1,4 +1,4 @@
-export const RESEARCH_REVIEW_DATE = 'September 5, 2026';
+export const RESEARCH_REVIEW_DATE = 'September 6, 2026';
 
 export const SOURCES = {
   pewTipping:
@@ -13,4 +13,14 @@ export const SOURCES = {
     'https://www.irs.gov/businesses/small-businesses-self-employed/tip-recordkeeping-and-reporting',
   emilyPost:
     'https://emilypost.com/advice/general-tipping-guide',
+  canadaGratuities:
+    'https://www.canada.ca/en/revenue-agency/services/tax/businesses/topics/gst-hst-businesses/charge-collect-special-cases.html',
+  visitBritain:
+    'https://www.visitbritain.com/en/plan-your-trip/useful-information',
+  tourismAustralia:
+    'https://www.australia.com/en/facts-and-planning/about-australia/key-facts-about-australia.html',
+  tourismNewZealand:
+    'https://www.newzealand.com/us/feature/new-zealand-currency/',
+  southAfricanTourism:
+    'https://www.southafrica.net/gl/en/trade/welcome/tip/tipping-culture-in-south-africa',
 };
